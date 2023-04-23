495d9fcbc5e3043e61d2cc5b09c5ee86295b70e0
AppleeX <125508968+appleexx-mhd@users.noreply.github.com>
2023-04-23 10:30:04 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -54,8 +54,8 @@ int _printf(const char *format, ...)
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buffer: Array
+ * @buff_ind: represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
